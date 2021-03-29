1b1f30670b71a703b1ec8ec1056286ed65a02d5e
laiagc892 <laiagc892@gmail.com>
2021-03-29 12:23:44 +0200

    basic fetch ex done

--- fetch.js
+++ fetch.js
@@ -1,1 +1,33 @@
 // Your code here
+
+var searchButton = document.querySelector("#searchButt");
+var userSearched = document.querySelector("#githubuser");
+var usersTable = document.querySelector("#myTable");
+
+searchButton.addEventListener("click", () => {
+    console.log(userSearched.value);
+
+    const username = userSearched.value;
+    fetchUser(username);
+})
+
+function fetchUser(username) {
+    const fetchUsersUrl = "https://api.github.com/users/";
+
+    fetch(`${fetchUsersUrl}${username}`).then((response) => {
+        return response.json();
+    }).then((responseJson) => {
+        console.log(responseJson);
+        addUserRow(responseJson);
+    })
+}
+
+function addUserRow(userInfo) {
+    usersTable.innerHTML += `<tr>
+                                    <td>${userInfo.login}</td>
+                                    <td><img src="${userInfo.avatar_url}" alt="avatar"></td>
+                                    <td>${userInfo.bio}</td>
+                                    <td>${userInfo.html_url}</td>
+                                </tr>`;
+}
+
